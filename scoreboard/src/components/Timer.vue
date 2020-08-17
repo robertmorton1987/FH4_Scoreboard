aea708aa8f98be20665a4fc22cceb9c4f5693956
Rob Morton <rmorton@trackermanagement.com>
2020-08-16 20:56:32 -0400

    Setup Car Class and Location setup and format fixes.

--- scoreboard/src/components/Timer.vue
+++ scoreboard/src/components/Timer.vue
@@ -1,12 +1,44 @@
 <template>
     <div class="w-full" align="center">
-        <div class="w-full pb-4">
-            <div class="w-40 bg-gray-500 text-black rounded-md text-6xl">{{ timeRemaining }}</div>
+        <div class="flex w-full pb-4">
+            <div class="flex w-1/3 justify-center items-end px-4">
+                <div class="flex text-xs font-bold text-gray-600">
+                    Car class (Chaser/Player):                              
+                </div>         
+            </div>
+            <div class="flex w-1/3 bg-gray-500 text-black rounded-md text-6xl justify-center">{{ timeRemaining }}</div>
+            <div class="flex w-1/3 justify-center items-end px-4">
+                <div class="flex text-xs font-bold text-gray-600">
+                    Location:                              
+                </div>         
+            </div>
         </div>
-        <div class="w-full pb-4">
-            <span v-if="showStart && !isRunning"><button class="w-1/4 bg-green-700 text-white rounded-md" @click="startTimer"> <font-awesome-icon :icon="['fas', 'stopwatch']" /> Start Timer</button></span>
-            <span v-else-if="showPause && isRunning"><button class="w-1/4 bg-red-700 text-white rounded-md" @click="pauseTimer"> <font-awesome-icon :icon="['fas', 'pause-circle']" /> Pause Timer</button></span>
-            <span v-if="!showStart && !isRunning"><button class="w-1/4 bg-green-700 text-white rounded-md" @click="continueTimer"><font-awesome-icon :icon="['fas', 'play-circle']" /> Continue Timer</button></span>
+        <div class="flex w-full">
+            <div class="flex w-1/3">
+                <div class="flex w-full mx-2">
+                    <select class="h-10 items-center text-sm block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" v-model="carClass">
+                        <option value="">Select a Car Class</option>
+                        <option v-for="option in carClasses" v-bind:key="option.value" >
+                            {{ option.text }}
+                        </option>
+                    </select>            
+                </div>            
+            </div>
+            <div class="flex w-1/3 justify-center">
+                <div v-if="showStart && !isRunning" class="w-full mx-2"><button class="w-full bg-green-700 text-white rounded-md" @click="startTimer"> <font-awesome-icon :icon="['fas', 'stopwatch']" /> Start Timer</button></div>
+                <div v-if="showPause && isRunning" class="w-full mx-2"><button class="w-full bg-red-700 text-white rounded-md" @click="pauseTimer"> <font-awesome-icon :icon="['fas', 'pause-circle']" /> Pause Timer</button></div>
+                <div v-if="!showStart && !isRunning" class="w-full mx-2"><button class="w-full bg-green-700 text-white rounded-md" @click="continueTimer"><font-awesome-icon :icon="['fas', 'play-circle']" /> Continue Timer</button></div>
+            </div>
+            <div class="flex w-1/3 justify-center">
+                <div class="flex w-full mx-2">
+                    <select class="h-10 items-center text-sm block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline" v-model="location">
+                        <option value="">Select a Location</option>
+                        <option v-for="option in locations" v-bind:key="option.value">
+                            {{ option.text }}
+                        </option>
+                    </select>            
+                </div>            
+            </div>
         </div>
         <div v-if="isRunning || showPause" class="w-full">
             <button class="w-1/4 text-red-700 bg-white hover:bg-red-700 hover:text-white rounded-md" @click="cancelTimer"> <font-awesome-icon :icon="['fas', 'ban']" /> Cancel</button>
@@ -29,6 +61,23 @@ export default {
     data() {
         return {
             timeRemaining: "",
+            carClass: "",
+            location: "",
+
+            carClasses: [
+                { text: "C/D", value: "C/D" },
+                { text: "B/C", value: "B/C" },
+                { text: "A/B", value: "A/B" },
+                { text: "S1/A", value: "S1/A" },
+                { text: "S2/S1", value: "S2/S1" },
+                { text: "X/S2", value: "X/S2" }
+            ],
+            locations: [
+                { text: "Ambleside", value: "A" },
+                { text: "Broadway", value: "B" },
+                { text: "Edinburgh", value: "E" }
+            ],
+
             startTime: 1200000,
             currentTime: 0,
 
